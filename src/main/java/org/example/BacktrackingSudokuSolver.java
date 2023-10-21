bb40d0acb5d0a757d6da38785017f2b9ad14a300
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-10-21 12:44:08 +0200

    Przeniesienie rozwiązywania sudoku do klasy BacktrackingSudokuSolver i jej lekka modyfikacja by działała na geterach i seterach zachowując prywatość adresu tablicy

--- src/main/java/org/example/BacktrackingSudokuSolver.java
+++ src/main/java/org/example/BacktrackingSudokuSolver.java
@@ -5,23 +5,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class BacktrackingSudokuSolver implements SudokuSolver {
-    @Override
-    public void solve(SudokuBoard board) {
 
-    }
+    public static final int GRID_SIZE = 9;
+
     private static final List<Integer> LIST = new ArrayList<>();
-
-    public static  final int GRID_SIZE = 9;
-
-    private int[][] board = new int[GRID_SIZE][GRID_SIZE];
-
-    public int[][] getBoard() {
-        int [][] copy = new int[GRID_SIZE][];
-        for (int i = 0; i < GRID_SIZE; i++) {
-            copy[i] = board[i].clone();
-        }
-        return copy;
-    }
 
     static {
         for (int i = 1; i <= GRID_SIZE; i++) {
@@ -29,38 +16,43 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
         }
     }
 
-    private void fillArrayWithZero() {
+    @Override
+    public void solve(SudokuBoard board) {
+        fillBoard(board);
+    }
+
+    private void fillArrayWithZero(SudokuBoard board) {
         for (int i = 0; i < GRID_SIZE; i++) {
             for (int j = 0; j < GRID_SIZE; j++) {
-                board[i][j] = 0;
+                board.set(i, j, 0);
             }
         }
     }
 
-    private boolean isNumberInRow(int number, int row) {
+    private boolean isNumberInRow(SudokuBoard board, int number, int row) {
         for (int i = 0; i < GRID_SIZE; i++) {
-            if (board[row][i] == number) {
+            if (board.get(row, i) == number) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isNumberInColumn(int number, int column) {
+    private boolean isNumberInColumn(SudokuBoard board, int number, int column) {
         for (int i = 0; i < GRID_SIZE; i++) {
-            if (board[i][column] == number) {
+            if (board.get(i, column) == number) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean isNumberInSquare(int number, int column, int row) {
+    private boolean isNumberInSquare(SudokuBoard board, int number, int column, int row) {
         int localBoxRow = row - row % 3;
         int localBoxColumn = column - column % 3;
         for (int i = localBoxRow; i < localBoxRow + 3; i++) {
             for (int j = localBoxColumn; j < localBoxColumn + 3; j++) {
-                if (board[i][j] == number) {
+                if (board.get(i, j) == number) {
                     return true;
                 }
             }
@@ -68,23 +60,23 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
         return false;
     }
 
-    private boolean isValid(int number, int column, int row) {
-        return !isNumberInColumn(number, column)
-                && !isNumberInRow(number, row)
-                && !isNumberInSquare(number, column, row);
+    private boolean isValid(SudokuBoard board, int number, int column, int row) {
+        return !isNumberInColumn(board, number, column)
+                && !isNumberInRow(board, number, row)
+                && !isNumberInSquare(board, number, column, row);
     }
 
-    private boolean solveBoard() {
+    private boolean solveBoard(SudokuBoard board) {
         for (int row = 0; row < GRID_SIZE; row++) {
             for (int column = 0; column < GRID_SIZE; column++) {
-                if (board[row][column] == 0) {
+                if (board.get(row, column) == 0) {
                     for (Integer numberToTry : LIST) {
-                        if (isValid(numberToTry, column, row)) {
-                            board[row][column] = numberToTry;
-                            if (solveBoard()) {
+                        if (isValid(board, numberToTry, column, row)) {
+                            board.set(row, column, numberToTry);
+                            if (solveBoard(board)) {
                                 return true;
                             } else {
-                                board[row][column] = 0;
+                                board.set(row, column, 0);
                             }
                         }
                     }
@@ -95,23 +87,13 @@ public class BacktrackingSudokuSolver implements SudokuSolver {
         return true;
     }
 
-    public  void printArray() {
-        for (int i = 0; i < GRID_SIZE; i++) {
-            for (int j = 0; j < GRID_SIZE; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    public void fillBoard() {
-        fillArrayWithZero();
+    public void fillBoard(SudokuBoard board) {
+        fillArrayWithZero(board);
         Collections.shuffle(LIST);
-        if (solveBoard()) {
+        if (solveBoard(board)) {
             System.out.println("Sudoku Board has been created");
         } else {
             System.out.println("Error while creating Sudoku Board");
         }
-        //printArray();
     }
 }
