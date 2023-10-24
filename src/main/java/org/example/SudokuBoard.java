392e814545cfa8d44185a47942ab08117afbec72
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-10-24 10:54:00 +0200

    usunięcie zbędnego przekazywania board po fukcjach w SudokuBoard

--- src/main/java/org/example/SudokuBoard.java
+++ src/main/java/org/example/SudokuBoard.java
@@ -42,13 +42,13 @@ public class SudokuBoard {
 
     private boolean testIfBoardIsCorrect() {
         for (int i = 0; i < SudokuBoard.GRID_SIZE; i++) {
-            if (!isNumberInRowTest(board,i) || !isNumberInColumnTest(board,i)) {
+            if (!isNumberInRowTest(i) || !isNumberInColumnTest(i)) {
                 return false;
             }
         }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (!isNumberInSquareTest(board,i * 3,j * 3)) {
+                if (!isNumberInSquareTest(i * 3,j * 3)) {
                     return false;
                 }
             }
@@ -56,7 +56,7 @@ public class SudokuBoard {
         return true;
     }
 
-    private boolean isNumberInRowTest(int[][] board, int row) {
+    private boolean isNumberInRowTest( int row) {
         boolean[] seen = new boolean[9];
         for (int i = 0; i < 9; i++) {
             if (board[row][i] < 1 || board[row][i] > 9 || seen[board[row][i] - 1]) {
@@ -67,7 +67,7 @@ public class SudokuBoard {
         return true;
     }
 
-    private boolean isNumberInColumnTest(int[][] board, int column) {
+    private boolean isNumberInColumnTest(int column) {
         boolean[] seen = new boolean[9];
         for (int i = 0; i < 9; i++) {
             if (board[i][column] < 1 || board[i][column] > 9 || seen[board[i][column] - 1]) {
@@ -78,7 +78,7 @@ public class SudokuBoard {
         return true;
     }
 
-    private boolean isNumberInSquareTest(int[][] board, int row, int column) {
+    private boolean isNumberInSquareTest(int row, int column) {
         boolean[] seen = new boolean[9];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
