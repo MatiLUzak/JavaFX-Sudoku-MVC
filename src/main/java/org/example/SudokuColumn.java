83e37ccbe2b05d8fd5cba826ba4a169653d46457
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-10-29 16:37:48 +0100

    Dodanie sprawdzania kolumn do SudokuColumn

--- src/main/java/org/example/SudokuColumn.java
+++ src/main/java/org/example/SudokuColumn.java
@@ -1,8 +1,22 @@
 package org.example;
 
 public class SudokuColumn implements SudokuStructure{
+    private SudokuField[] fields;
+
+    public SudokuColumn(SudokuField[] fields) {
+        this.fields = fields;
+    }
+
     @Override
     public boolean verify() {
-        return false;
+        boolean[] seen = new boolean[9];
+        for (SudokuField field : fields) {
+            int value = field.getValue();
+            if (value < 1 || value > 9 || seen[value - 1]) {
+                return false;
+            }
+            seen[value - 1] = true;
+        }
+        return true;
     }
 }
