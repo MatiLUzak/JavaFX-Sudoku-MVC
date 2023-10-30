e6ff8610f161172cfd0d51b3e4547f6a9bd545f6
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-10-30 18:28:07 +0100

    dodanie obserwatora do SudokuField

--- src/main/java/org/example/SudokuField.java
+++ src/main/java/org/example/SudokuField.java
@@ -1,13 +1,31 @@
 package org.example;
 
+import java.beans.PropertyChangeSupport;
+import java.beans.PropertyChangeListener;
+
 public class SudokuField {
     private int value;
+    private PropertyChangeSupport support;
+
+    public SudokuField() {
+        support =new PropertyChangeSupport(this);
+    }
 
     public int getValue() {
         return value;
     }
 
     public void setValue(int value) {
+        int oldValue = this.value;
         this.value = value;
+        support.firePropertyChange("value",oldValue,this.value);
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        support.removePropertyChangeListener(listener);
     }
 }
