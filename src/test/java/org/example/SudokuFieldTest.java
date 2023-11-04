014fcad041f0080c5c12a56471e570b83110fdaf
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-11-04 11:44:24 +0100

    dodatnie testów SudokuFieldTest

--- src/test/java/org/example/SudokuFieldTest.java
+++ src/test/java/org/example/SudokuFieldTest.java
@@ -2,4 +2,10 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class SudokuFieldTest {
+    @Test
+    void valueIsSetAndGetCorrectly() {
+        SudokuField field = new SudokuField();
+        field.setValue(5);
+        assertEquals(5, field.getValue());
+    }
 }
