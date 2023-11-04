6942ba3c5b7895f6b71fe3c1ccdb3f1b172805a2
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-11-04 11:06:22 +0100

    stworzenie testu SudokuColumnTest

--- src/test/java/org/example/SudokuColumnTest.java
+++ src/test/java/org/example/SudokuColumnTest.java
@@ -1,4 +1,25 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SudokuColumnTest {
+    @Test
+    void constructorShouldThrowExceptionForNullArray() {
+        assertThrows(IllegalArgumentException.class, () -> new SudokuColumn(null));
+    }
+    @Test
+    void TestVerifyRow(){
+        SudokuField[] fields = new SudokuField[SudokuBoard.GRID_SIZE];
+        for(int i = 0; i < fields.length; i++){
+            fields[i]=new SudokuField();
+            fields[i].setValue(i+1);
+        }
+        SudokuColumn column = new SudokuColumn(fields);
+        assertTrue(column.verify());
+        fields[0].setValue(0);
+        fields[1].setValue(0);
+        assertFalse(column.verify());
+    }
 }
