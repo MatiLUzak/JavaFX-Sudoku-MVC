67d8ef4ca048bcdb68046b2c6e837d048ab9d287
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-11-04 11:26:45 +0100

    zakomentowanie main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         SudokuSolver solver = new BacktrackingSudokuSolver();
         SudokuBoard board = new SudokuBoard(solver);
         board.solveGame();
@@ -17,6 +17,6 @@ public class Main {
             }
             System.out.println();
         }
-    }
+    }*/
 
 }
