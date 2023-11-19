c47e79f009bbf5da13137d41feced9e3076264ad
247722 Luzak Mateusz <247722@edu.p.lodz.pl>
2023-11-19 13:03:31 +0100

    dodanie testów do SudokuField

--- src/test/java/org/example/SudokuFieldTest.java
+++ src/test/java/org/example/SudokuFieldTest.java
@@ -8,4 +8,49 @@ public class SudokuFieldTest {
         field.setValue(5);
         assertEquals(5, field.getValue());
     }
+
+    @Test
+    void testToString() {
+        SudokuField field = new SudokuField();
+        field.setValue(5);
+        String expected = "SudokuField[value=5]";
+        String actual = field.toString();
+        assertTrue(actual.contains("value=5"), "toString should contain the correct value representation");
+    }
+
+    @Test
+    void testEqualsSelf() {
+        SudokuField field = new SudokuField();
+        assertTrue(field.equals(field), "A field should be equal to itself.");
+    }
+
+    @Test
+    void testEqualsDifferentValue() {
+        SudokuField field1 = new SudokuField();
+        field1.setValue(5);
+        SudokuField field2 = new SudokuField();
+        field2.setValue(6);
+        assertFalse(field1.equals(field2), "Fields with different values should not be equal.");
+    }
+
+    @Test
+    void testEqualsNull() {
+        SudokuField field = new SudokuField();
+        assertFalse(field.equals(null), "A field should not be equal to null.");
+    }
+
+    @Test
+    void testEqualsDifferentClass() {
+        SudokuField field = new SudokuField();
+        assertFalse(field.equals(new Object()), "A field should not be equal to an object of a different class.");
+    }
+
+    @Test
+    void testHashCodeConsistentWithEquals() {
+        SudokuField field1 = new SudokuField();
+        field1.setValue(5);
+        SudokuField field2 = new SudokuField();
+        field2.setValue(5);
+        assertEquals(field1.hashCode(), field2.hashCode(), "Hash codes should be the same for equal objects.");
+    }
 }
